a5c42a211764711ee4fce602454a4ac23cb03f6d
Asdrubal Hernandez <ahernand.development@gmail.com>
2021-05-11 15:27:11 -0500

    adding controllers and views for Curso model

---
+++ EscuelaApp/Controllers/CursoController.cs
@@ -0,0 +1,46 @@
+﻿using EscuelaApp.Data;
+using EscuelaApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EscuelaApp.Controllers
+{
+    [Route("/cursos")]
+    public class CursoController : Controller
+    {
+        private EscuelaDbContext _context;
+
+        public CursoController(EscuelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+
+            var CURSOS = _context.Cursos.ToList();
+
+            return View(CURSOS);
+        }
+
+        [HttpGet("/cursos/detalles/{cursoId?}")]
+        public IActionResult Details(string cursoId)
+        {
+            if (string.IsNullOrEmpty(cursoId)) 
+            {
+                return RedirectToAction("Index");
+            }
+
+            var CURSO = from curso in _context.Cursos
+                         where curso.Id == cursoId
+                         select curso;
+
+            return View(CURSO.SingleOrDefault());
+        }
+
+    }
+
+}
